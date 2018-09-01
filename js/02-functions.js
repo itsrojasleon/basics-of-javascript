8d92e7f1a268d7abb28ecd35504cc0a2027fd1b4
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-08-31 20:54:25 -0500

    Maintain window cleared

--- js/02-functions.js
+++ js/02-functions.js
@@ -1,6 +1,14 @@
+// Basics
 let name = 'Luis', age = 19;
 
 function printAge(n , a) {
-  console.log(`${n} is ${a} years old`);
+  // console.log(`${n} is ${a} years old`);
 }
 printAge('Rojas', 19);
+
+// Better way
+function printNameUpperCase(name) {
+  name = name.toUpperCase();
+  console.log(name);
+}
+printNameUpperCase(name);
