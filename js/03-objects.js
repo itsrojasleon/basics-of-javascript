5a7312692a407b8c96c18e17aa6ea1879f5b2222
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-05 16:50:58 -0500

    Destructuring

--- js/03-objects.js
+++ js/03-objects.js
@@ -1,9 +1,17 @@
 let rojas = {
-  name: 'Juan Luis',
-  lastName: 'Rojas León',
-  edad: 28,
+  otherObject: {
+    name: 'Juan Luis',
+    lastName: 'Rojas León',
+    edad: 28,
+  }
 } 
 function printName({ lastName }) {
-  console.log(lastName);
+  // console.log(lastName);
 }
 printName(rojas);
+
+// Destructuring
+function print({ otherObject: { name, lastName } }) {
+  console.log(name);
+}
+print(rojas);
