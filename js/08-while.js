f4eb919c2e911150229b48a72cf28ae19ca170c1
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-07 19:39:48 -0500

    while added

--- js/08-while.js
+++ js/08-while.js
@@ -5,17 +5,23 @@ const rojas = {
   weight: 70
 }
 console.log(`Start of year ${rojas.name} weight ${rojas.weight}`);
-const WEIGHT = 0.2;
+const WEIGHT = 0.3;
 const gainWeight = person => person.weight += WEIGHT;
 const decreaseInWeight = person => person.weight -= WEIGHT;
+const eatALot = () => Math.random() < 0.3;
+const doExercise = () => Math.random() < 0.4;
+const GOAL = rojas.weight - 3;
+let days = 0;
 
-for(i = 0; i <= 360; i++) {
-  const random = Math.random();
-  if (random < 0.25) {
+while (rojas.weight > GOAL) {
+  if (eatALot()) {
     gainWeight(rojas);
-  }else if (random < 0.50) {
+  } else if (doExercise()) {
     decreaseInWeight(rojas);
   }
+  days += 1;
 }
+
+console.log(`${days} days passed`);
 
 console.log(`Start of year ${rojas.name} weight ${rojas.weight.toFixed(2)}`);
