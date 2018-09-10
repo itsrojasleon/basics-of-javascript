486f24320d500242e48e9bd8bfda941ccddd8628
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-10 18:59:10 -0500

    swtich

--- js/09-do-while.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const rains = () => Math.random() < 0.25;
-
-do {
-  count++;
-}while(!rains());
-
-console.log(`${count}`, count === 1 ? 'vez' : 'veces');
---
+++ js/10-switch.js
@@ -0,0 +1,10 @@
+const zodiacSign = prompt('zodiac sign');
+
+switch(zodiacSign) {
+  case 'acuario':
+    console.log('acuario');
+    break;
+  case 'piscis':
+    console.log('piscis');
+    break;
+}
