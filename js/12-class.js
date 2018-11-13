730e0515e0f7660db2b75fe36534a6a278a7f2d2
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-12 20:05:57 -0600

    this

--- js/12-class.js
+++ js/12-class.js
@@ -3,21 +3,12 @@ function Person(name, lastName, high) {
   this.lastName = lastName;
   this.high = high;
 }
-Person.prototype.saludar = function() {
+Person.prototype.greetings = function() {
   console.log(`Hi, my name's ${this.name} ${this.lastName}`)
 }
-Person.prototype.isHigh = function() {
-  if (this.high > 1.8) 
-    console.log(`${this.name} is Very high`);
-  else 
-    console.log(`${this.name} is normal`);
+Person.prototype.isHigh = () => {
+  return this.high > 1.8
 }
 const Rojas = new Person('Luis', 'Rojas', 1.9);
 const Pedro = new Person('Pedro', 'kuko', 1.7);
 const Stephen = new Person('Stephen', 'kuka', 2);
-
-
-// Rojas.saludar();
-Rojas.isHigh();
-Pedro.isHigh();
-Stephen.isHigh();
