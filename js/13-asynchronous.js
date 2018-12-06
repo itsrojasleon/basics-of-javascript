23223a9fb015f994da482f9294ecdf8eb91632e7
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-12-06 17:38:42 -0600

    Async await

--- js/13-asynchronous.js
+++ js/13-asynchronous.js
@@ -1,12 +1,24 @@
-console.log('a');
-// setTimeout(function() {
-//   console.log('b');
-// }, 0)
-setTimeout(() => {
-  console.log('print')
-}, 2000);
+const API_URL = 'https://swapi.co/api/';
+const PEOPLE = 'people/:id';
 
-for (let i = 0; i <= 10000000; i++) {
-  console.log('d');
+// const request = async (id) => {
+//   const res = await fetch(`${API_URL}${PEOPLE.replace(':id', id)}`, { crossDomain: true });
+//   const data = await res.json();
+//   return data;
+// }
+// Promise.all([request(1), request(2), request(3)]).then((data) => {
+//   console.log(data);
+// });
+
+const request = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await fetch(`${API_URL}${PEOPLE.replace(':id', id)}`, { crossDomain: true });
+      const data = await res.json();
+      resolve(data);
+      
+    }catch(e) {
+      reject(id);
+    }
+  });
 }
-// DON'T DO IT
